8af27a1b8229c6b16afaa3e08e31630952bcbe51
Jeremy <jeremy@fullstory.com>
2023-05-12 09:22:18 -0400

    fix: swapping in the absolute module reference

--- examples/europe/main.tf
+++ examples/europe/main.tf
@@ -1,5 +1,5 @@
 module "fullstory_relay" {
-  source      = "../.." #TODO: change to absolute reference
+  source      = "fullstorydev/fullstory-cloud-relay/google"
   relay_fqdn  = "fsrelay.your-company.com"
   target_fqdn = "eu1.fullstory.com"
 }
